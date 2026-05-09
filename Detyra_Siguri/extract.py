ec76107c6fdfbf9753729c1e93400c053c2155cb
Albion-Mazrekaj <albion.mazrekaj@student.uni-pr.edu>
2026-05-09 14:40:12 +0200

    Permirsime

--- Detyra_Siguri/extract.py
+++ Detyra_Siguri/extract.py
@@ -1,68 +1,63 @@
 from PIL import Image
 import random
 
-# Çelësi për randomizim (duhet të jetë i njëjtë si në embed)
+# 1. KONFIGURIMI I SISTEMIT
+# Çelësi për randomizimin e pikselave 
 key = 12345
 
-# Zgjedh kanalin e ngjyrës (0 = Red, 1 = Green, 2 = Blue)
+# Kanali i ngjyrës që është përdorur (0=Red, 1=Green, 2=Blue)
 colourPlane = 0
 
-# Biti që është përdorur për fshehje (LSB ose bit specifik)
+# Biti specifik ku është fshehur informacioni
 significantBit = 7
 
-# Emri i imazhit të steganografuar
+# Imazhi i steganografuar
 stegoImage = "stego-img.bmp"
 
-# Hap imazhin dhe e konverton në RGB format
+
+# 2. LEXIMI I IMAZHIT
 image = Image.open(stegoImage).convert("RGB")
-
-# Merr dimensionet e imazhit (gjerësia, lartësia)
 dimensions = image.size
-
-# Merr akses në pikselat e imazhit
 pixels = image.load()
 
-# Numri total i pikselave në imazh
 total_pixels = dimensions[0] * dimensions[1]
 
-# Krijon listë me indekset e pikselave
-shuffledIndices = list(range(total_pixels))
 
-# Përzien indeksat në mënyrë të rastësishme me seed (çelës)
+# 3. RIKRIJIMI I RENDIT TË PIKSELAVE
+shuffledIndices = list(range(total_pixels))
 random.seed(key)
 random.shuffle(shuffledIndices)
 
-# Këtu do ruhen bitët e nxjerrë nga imazhi
+
+# 4. NXJERRJA E BIT-AVE
 extractedBits = []
 
-# Nxjerr bitin e fshehur nga çdo pixel sipas rendit të përzier
 for i in shuffledIndices:
-    x = i % dimensions[0]   # koordinata X
-    y = i // dimensions[0]  # koordinata Y
 
-    # Merr vlerën e ngjyrës dhe e kthen në binar (8-bit)
-    p = format(pixels[x, y][colourPlane], '08b')
+    x = i % dimensions[0]
+    y = i // dimensions[0]
 
-    # Merr bitin e fshehur (LSB ose bit i zgjedhur)
-    extractedBits.append(p[significantBit])
+    pixel = pixels[x, y]
+    value = pixel[colourPlane]
 
-# Nxjerr gjatësinë e mesazhit (14 bitët e parë)
-extractedLength = int(''.join(extractedBits[:14]), 2)
+    # nxjerr bitin e zgjedhur (LSB)
+    bit = (value >> significantBit) & 1
+    extractedBits.append(str(bit))
 
-# String për mesazhin e rikuperuar
-secret = ''
 
-# Rikonstrukton mesazhin karakter për karakter
-for i in range(extractedLength):
-    # Merr 7 bitët për çdo karakter
+# 5. NXJERRJA E GJATËSISË SË MESAZHIT
+length_bits = ''.join(extractedBits[:14])
+message_length = int(length_bits, 2)
+
+
+# 6. RIKONSTRUKTIMI I MESAZHIT
+secret = ""
+
+for i in range(message_length):
     char_bits = extractedBits[14 + i*7 : 14 + (i+1)*7]
-
-    # Kthen binarin në numër
     value = int(''.join(char_bits), 2)
-
-    # Kthen numrin në karakter ASCII
     secret += chr(value)
 
-# Shfaq mesazhin e fshehur
+# 7. OUTPUT
 print("Recovered message:")
 print(secret)
